6e30e8e8f4f2e8707b4d74a874491a870d795d3d
ad289010 <89672725+ad289010@users.noreply.github.com>
2021-08-27 23:42:39 -0400

    commit

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '384333203077776' );
+	define( 'INSTAGRAM_APP_SECRET', '957dab50b709d251f28f7ed6d6b4cd74' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://ad289010.github.io/' );
